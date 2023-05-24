565588a97df2e7e6f441453b02f80984afdde211
hazadus <hazadus7@gmail.com>
2023-05-24 17:49:56 +0300

    tests: created test for `/health_check` endpoint.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::dev::Server;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("Rust World");
@@ -6,17 +7,18 @@ async fn greet(req: HttpRequest) -> impl Responder {
 }
 
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("{name}", web::get().to(greet))
     })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    .bind("127.0.0.1:8000")?
+    .run();
+
+    Ok(server)
 }
