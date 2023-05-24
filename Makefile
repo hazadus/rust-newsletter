c61a9ba433fc1bc043ec2ffae6cdc174985b677a
hazadus <hazadus7@gmail.com>
2023-05-24 18:20:17 +0300

    refactor: code reformatted.

--- Makefile
+++ Makefile
@@ -3,4 +3,9 @@ release:
 docs:
 	cargo doc --open
 expand:
-	 cargo +nightly expand
+	cargo +nightly expand
+fmt:
+	cargo fmt
+test:
+	make fmt
+	cargo test
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("Rust World");
