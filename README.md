244833059fc60b09de05ca534c383a9ee2994962
hazadus <hazadus7@gmail.com>
2023-05-24 19:28:51 +0300

    dev, tests: basic `/subscriptions` endpoint implemented.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # rust-newsletter
 
 Simple newsletter app, written in Rust.
+
+## References
+ - [Actix Documentation](https://actix.rs/docs)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -16,7 +16,7 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-/// Launch the application in background
+/// Launch the application in background.
 /// Bind TCP listener to random port, and return the address.
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
@@ -25,4 +25,49 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn subscribe_return_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=hazadus&email=hazadus7%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_return_400_for_missing_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=hazadus", "missing the email"),
+        ("email=hazadus%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional custom error message on test failure:
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
