b6489a7d76da812ffca6a3369a994bce4c29d040
hazadus <hazadus7@gmail.com>
2023-05-27 00:00:55 +0300

    dev: tracing instrumentation added.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use tracing::Instrument;
 
 /// Form data shape for `subscribe` endpoint.
 #[derive(serde::Deserialize)]
@@ -24,13 +25,18 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     // We use UUID to correlate al log messages to the request.
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {} - Saving new subscriber in DB: name: {}, email: {}",
-        request_id,
-        form.name,
-        form.email
+    let request_span = tracing::info_span!(
+        "Adding new subscriber.",
+        %request_id,
+        subscriber_name = %form.name,
+        subscriber_email =  %form.email
     );
 
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database."
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -41,22 +47,15 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
+    .execute(pool.as_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved.",
-                request_id
-            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
