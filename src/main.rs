a0bc936a904f521d138cceb9249bae112c649d0c
hazadus <hazadus7@gmail.com>
2023-05-27 19:14:37 +0300

    dev: tracing output in JSON format implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use env_logger::Env;
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // We are falling back to printing all logs at info-level or above
+    // We are falling back to printing all spans at info-level or above
     // if the `RUST_LOG` environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "newsletter".into(),
+        // Output the formatted spans to `stdout`:
+        std::io::stdout
+    );
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    // Specify that our `subscriber` should be used to process spans
+    set_global_default(subscriber).expect("Failed to set subscriber.");
 
     // Panic if we can't read config file
     let configuration = get_configuration().expect("Failed to read config file.");
