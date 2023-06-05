d6eddc5458e919650d06a043171c9ed271497a88
hazadus <hazadus7@gmail.com>
2023-06-05 21:34:02 +0300

    refactor: 7.3.8.1 Extracting Startup Code to `build()`.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use newsletter::configuration::get_configuration;
 use newsletter::database::configure_db_if_not_exists;
-use newsletter::email_client::EmailClient;
-use newsletter::startup::run;
+use newsletter::startup::build;
 use newsletter::telemetry::{get_subscriber, init_subscriber};
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -15,29 +12,9 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read config file.");
 
     configure_db_if_not_exists(&configuration.database).await;
-    let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(5))
-        .connect_lazy_with(configuration.database.with_db());
 
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address).expect("Failed to bind address.");
-
-    run(listener, connection_pool, email_client)?.await?;
+    let server = build(configuration).await?;
+    server.await?;
 
     Ok(())
 }
